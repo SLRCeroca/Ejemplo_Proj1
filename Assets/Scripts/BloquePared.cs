0f30a2ba7d8bcf5628856f62cdaa4ac8cf77ff5f
Sergio López Ramos <sergiolopez@ceroca.cat>
2025-10-02 17:03:36 +0200

    Bloques Paredes Hechos

---
+++ Assets/Scripts/BloquePared.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BloquePared : MonoBehaviour
+{
+    private float vel = 20f;
+    private const float LIMIT_Z_NEGATIVO = -10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y,
+            transform.position.z - vel * Time.deltaTime
+        );
+        if(transform.position.z < LIMIT_Z_NEGATIVO){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GeneradorBloquePared.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class GeneradorBloquePared : MonoBehaviour
+{
+    public float vel = 20f;
+    private const float LIMITE_IZQUIERDA = -6f;
+    private const float LIMITE_INFERIOR = -1f;
+    private const float LIMITE_DERECHA = 6f;
+    private const float LIMITE_SUPERIOR = 6f;
+    private const float LIMITE_POSTERIOR = 90f;
+
+    public GameObject prefabBloquePared;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Param1: Funcion que se llama repetidamente
+        //Param2: Cuanto tarda en llamarse desde que se inicia la escena
+        //Param3: Cada cuanto tiempo se vuelve a llamar
+        InvokeRepeating("GenerarBloquesPared", 0.5f, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void GenerarBloquesPared()
+    {
+        GameObject ParedInferiorIzquierda = Instantiate(prefabBloquePared);
+        ParedInferiorIzquierda.transform.position = new Vector3(
+            LIMITE_IZQUIERDA,
+            LIMITE_INFERIOR,
+            LIMITE_POSTERIOR
+        );
+        GameObject ParedInferiorDerecha = Instantiate(prefabBloquePared);
+        ParedInferiorDerecha.transform.position = new Vector3(
+            LIMITE_DERECHA,
+            LIMITE_INFERIOR,
+            LIMITE_POSTERIOR
+        );
+        GameObject ParedSuperiorIzquierda = Instantiate(prefabBloquePared);
+        ParedSuperiorIzquierda.transform.position = new Vector3(
+            LIMITE_IZQUIERDA,
+            LIMITE_SUPERIOR,
+            LIMITE_POSTERIOR
+        );
+        GameObject ParedSuperiorDerecha = Instantiate(prefabBloquePared);
+        ParedSuperiorDerecha.transform.position = new Vector3(
+            LIMITE_DERECHA,
+            LIMITE_SUPERIOR,
+            LIMITE_POSTERIOR
+        );
+    }
+}
