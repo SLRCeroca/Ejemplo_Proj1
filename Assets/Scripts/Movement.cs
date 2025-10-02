ad033d6d3822f93260d2de6d4d0138e3aeaf9d31
Sergio López Ramos <sergiolopez@ceroca.cat>
2025-10-02 15:57:44 +0200

    Movimiento y Limite Pantalla

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,10 +5,16 @@ public class Movement : MonoBehaviour
 {
     public float vel = 10f;
     private Camera camera;
+    private Vector3 limiteInferiorIzquierda;
+    private Vector3 limiteSuperiorDerecha;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         camera = Camera.main;
+        //Buscamos la distancia entre camara y objeto que no queremos que salga del viewport en el axis Z
+        float distanciaZCameraNave = Mathf.Abs(transform.position.z - camera.transform.position.z);
+        limiteInferiorIzquierda = camera.ViewportToWorldPoint(new Vector3(0,0, distanciaZCameraNave));
+        limiteSuperiorDerecha = camera.ViewportToWorldPoint(new Vector3(1,1, distanciaZCameraNave));
     }
 
     // Update is called once per frame
@@ -20,9 +26,6 @@ public class Movement : MonoBehaviour
 
     void MovimientoNave()
     {
-        //Control limites pantalla
-        Vector3 limiteInferiorIzquierda = camera.ViewportToWorldPoint(new Vector2(0,0));
-        Vector3 limiteSuperiorDerecha = camera.ViewportToWorldPoint(new Vector2(1,1));
         //Movimiento nave
         float direccionHorizontal = Input.GetAxisRaw("Horizontal");
         float direccionVertical = Input.GetAxisRaw("Vertical");
@@ -48,6 +51,15 @@ public class Movement : MonoBehaviour
 
     void ControlLimitesPantalla()
     {
-        
+        //No podemos modificar el transform.position.x, solo consultarlo, asi que hacemos una nueva variable
+        Vector3 nuevaPosicion = transform.position;
+        //Control de los limites de la pantalla
+        //El metodo Math.Clamp nos devuelve el primer parametro, pero si el primer parametro es mas pequeño que el segundo devuelve el segundo,
+        //Pero si tiene un valor mas grande que el tercero nos devuelve el tercer parametro.
+        nuevaPosicion.x = Math.Clamp(nuevaPosicion.x, limiteInferiorIzquierda.x, limiteSuperiorDerecha.x);
+        nuevaPosicion.y = Math.Clamp(nuevaPosicion.y, limiteInferiorIzquierda.y, limiteSuperiorDerecha.y);
+
+        //Aplicamos la posicion
+        transform.position = nuevaPosicion;
     }
 }
